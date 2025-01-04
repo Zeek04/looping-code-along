63eadef0c780beb2a25d35106d61383da2efda64
Ezequiel <cruzezequiel81@gmail.com>
2025-01-03 22:57:31 -0500

    Finished Looping Code Along

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(name, event){
+
+    const names = []
+
+    for(let i = 0; name.length > i ; i++){
+        names.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return names
+
+}
+
+function countDown(number){
+
+    while(number > -1){
+        console.log(number--)
+    }
+    
+}
+
+countDown(30)
